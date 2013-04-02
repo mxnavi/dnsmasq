5873a8cbe9a9a4301feee7a19b5ac4ae2d68f787
Sergii Kurylo <x0156751@ti.com>
2013-04-02 11:42:26 +0000

    WFD: Activate DHCP script support for dnsmasq
    This script is needed to get IP address of the remote
    device in case our device runs DHCP server.
    
    Change-Id: I5f68e0671e0b5cb7145b666bf1a27f0d046262c6
    Signed-off-by: Sergii Kurylo <x0156751@ti.com>

--- src/Android.mk
+++ src/Android.mk
@@ -10,7 +10,12 @@ LOCAL_MODULE := dnsmasq
 
 LOCAL_C_INCLUDES := external/dnsmasq/src
 
+ifeq ($(OMAP_ENHANCEMENT), true)
+LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -DNO_IPV6 -DNO_TFTP
+else
 LOCAL_CFLAGS := -O2 -g -W -Wall -D__ANDROID__ -DNO_IPV6 -DNO_TFTP -DNO_SCRIPT
+endif
+
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc libcutils
 
 include $(BUILD_EXECUTABLE)
